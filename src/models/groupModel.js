810c3f019bccaa19d527bec18215bcd3bf43bff3
Slava <ogurezslava@gmail.com>
2022-09-29 12:28:39 +0100

    HW-4 Try to fix add users to group (#7)

--- src/models/groupModel.js
+++ src/models/groupModel.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const { sequelize } = require('../db');
+const sequelize  = require('../db');
 
 const Group = sequelize.define('group', {
     id: {
--- src/models/userGroupsModel.js
+++ src/models/userGroupsModel.js
@@ -1,8 +1,14 @@
-const { sequelize } = require('../db');
+const  sequelize = require('../db');
+const {User } = require('./userModel')
+const {Group} = require('./groupModel')
 
 const UserGroups = sequelize.define('users_groups', {}, {
-    timestamps: false
+    timestamps: false,
+    tableName: 'user_groups'
 });
+
+User.belongsToMany(Group, { through: UserGroups });
+Group.belongsToMany(User, { through: UserGroups });
 
 
 module.exports = { UserGroups };
