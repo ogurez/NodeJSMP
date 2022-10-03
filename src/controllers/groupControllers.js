fb62634a4721eb77187bca59fba489a660988a87
Slava <ogurezslava@gmail.com>
2022-10-03 16:24:38 +0100

    HW-5 Add logger

--- src/controllers/groupControllers.js
+++ src/controllers/groupControllers.js
@@ -1,57 +1,79 @@
 const GroupService = require('../services/groupService');
+const { logger } = require('../logger');
 
 const gs = new GroupService();
 
 const getGroup = async (req, res) => {
-    const { id } = req.params;
-    const group = await gs.Get(id);
+    try {
+        const { id } = req.params;
+        const group = await gs.Get(id);
 
-    if (!group) {
-        res.status(404).json({ message: `Group with id ${id} not found` });
-    } else {
-        res.status(200).json(group);
+        if (!group) {
+            res.status(404).json({ message: `Group with id ${id} not found` });
+        } else {
+            res.status(200).json(group);
+        }
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
 };
 
 const getAllGroups = async (req, res) => {
-    const groups = await gs.GetAll();
+    try {
+        const groups = await gs.GetAll();
 
-    if (!groups.length) {
-        res.status(404).json({ message: 'No groups exsisted' });
-    } else {
-        res.status(200).json(groups);
+        if (!groups.length) {
+            res.status(404).json({ message: 'No groups exsisted' });
+        } else {
+            res.status(200).json(groups);
+        }
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
 };
 
 const createGroup = async (req, res) => {
-    const group = await gs.Create(req.body);
-    res.status(201).json(group);
+    try {
+        const group = await gs.Create(req.body);
+        res.status(201).json(group);
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
+    }
 };
 
 const updateGroup = async (req, res) => {
-    const { id } = req.params;
-    const group = await gs.Get(id);
+    try {
+        const { id } = req.params;
+        const group = await gs.Get(id);
 
-    if (!group) {
-        res.status(404).json({ message: `Group with id ${id} not found` });
+        if (!group) {
+            res.status(404).json({ message: `Group with id ${id} not found` });
+        }
+
+        await gs.Update(req.body, id);
+
+        res.status(200).json(`Group with id ${id} updated successfully`);
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
-
-    await gs.Update(req.body, id);
-
-    res.status(200).json(`Group with id ${id} updated successfully`);
 };
 
 const deleteGroup = async (req, res) => {
-    const { id } = req.params;
-    const group = await gs.Get(id);
+    try {
+        const { id } = req.params;
+        const group = await gs.Get(id);
 
-    if (!group) {
-        res.status(404).json({ message: `Group with id ${id} not found` });
+        if (!group) {
+            res.status(404).json({ message: `Group with id ${id} not found` });
+        }
+
+        await gs.Delete(id);
+
+        res.status(204).send();
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
-
-    await gs.Delete(id);
-
-    res.status(204).send();
 };
+
 
 module.exports = { getGroup, getAllGroups, createGroup, deleteGroup, updateGroup };
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,74 +1,99 @@
 const UserService = require('../services/userService');
+const { logger } = require('../logger');
 
 const us = new UserService();
 
 const getUser = async (req, res) => {
-    const { id } = req.params;
-    const user = await us.Get(id);
+    try {
+        const { id } = req.params;
+        const user = await us.Get(id);
 
-    if (!user) {
-        res.status(404).json({ message: `User with id ${id} not found` });
-    } else {
-        res.status(200).json(user);
+        if (!user) {
+            res.status(404).json({ message: `User with id ${id} not found` });
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
 };
 
 const getAutoSuggestedUsers = async (req, res) => {
-    const { loginSubString, limit } = req.params;
-    const users = await us.GetSuggested(loginSubString, limit);
+    try {
+        const { loginSubString, limit } = req.params;
+        const users = await us.GetSuggested(loginSubString, limit);
 
-    if (!users.length) {
-        res.status(404).json({ message: 'No users were found' });
-    } else {
-        res.status(200).json(users);
+        if (!users.length) {
+            res.status(404).json({ message: 'No users were found' });
+        } else {
+            res.status(200).json(users);
+        }
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
 };
 
 const createUser = async (req, res) => {
-    const user = await us.Create(req.body);
-    res.status(201).json(user);
+    try {
+        const user = await us.Create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
+    }
 };
 
 const updateUser = async (req, res) => {
-    const { id } = req.params;
-    const user = await us.Get(id);
+    try {
+        const { id } = req.params;
+        const user = await us.Get(id);
 
-    if (!user) {
-        res.status(404).json({ message: `User with id ${id} not found` });
+        if (!user) {
+            res.status(404).json({ message: `User with id ${id} not found` });
+        }
+
+        await us.Update(req.body, id);
+
+        res.status(200).json(`User with id ${id} updated successfully`);
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
-
-    await us.Update(req.body, id);
-
-    res.status(200).json(`User with id ${id} updated successfully`);
 };
 
 const deleteUser = async (req, res) => {
-    const { id } = req.params;
-    const user = await us.Get(id);
+    try {
+        const { id } = req.params;
+        const user = await us.Get(id);
 
-    if (!user) {
-        res.status(404).json({ message: `User with id ${id} not found` });
+        if (!user) {
+            res.status(404).json({ message: `User with id ${id} not found` });
+        }
+
+        await us.Delete(id);
+
+        res.status(204).send();
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
-
-    await us.Delete(id);
-
-    res.status(204).send();
 };
 
 const addUsersToGroup = async (req, res) => {
-    const { group_id, users_id } = req.body;
+    try {
+        const { group_id, users_id } = req.body;
 
-    if (!group_id) {
-        res.status(404).json({ message: `Group with id ${group_id} not found` });
+        if (!group_id) {
+            res.status(404).json({ message: `Group with id ${group_id} not found` });
+        }
+
+        if (!users_id) {
+            res.status(404).json({ message: 'Please provide user ids which you want to add to the group' });
+        }
+
+        await us.AddUsersToGroup(group_id, users_id);
+
+        res.status(204).send();
+    } catch (err) {
+        logger.error(`${req.method} ${req.params} ${err.message}`);
     }
-
-    if (!users_id) {
-        res.status(404).json({ message: 'Please provide user ids which you want to add to the group' });
-    }
-
-    await us.AddUsersToGroup(group_id, users_id);
-
-    res.status(204).send();
 };
 
 module.exports = { getUser, getAutoSuggestedUsers, createUser, deleteUser, updateUser, addUsersToGroup };
